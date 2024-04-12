7fb5e7a76cebb0c72f24ae9c2bb813d6e7f62175
Pooja More <131580513+poojamore24@users.noreply.github.com>
2024-04-12 05:45:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const accessKey = "Fb4XQzGJk7DNcqfh0E0dkjcCTB9FG4DH3daQ8N7A1sc"
+
+const formEl = document.querySelector("form")
+// const inputEl = document.getElementById("#search-input")
+const inputE1 = document.getElementById("search-input");
+
+const searchResults  = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button")
+
+let inputData = "";
+let page = 1;
+
+
+ async function searchImages(){
+  inputData = inputE1.value;
+  // const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}';
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+
+  const response =await fetch(url);
+  const data =await response.json();
+
+  const results = data.results;
+
+  if (page === 1){
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result)=> {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target= "_blank";
+    imageLink.textContent = result.alt_description;
+
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+
+  page++
+  if(page > 1){
+    showMore.style.display ="block";
+  }
+}
+
+
+formEl.addEventListener("submit",(event) =>{
+
+  event.preventDefault();
+  page = 1;
+  searchImages();
+});
+
+showMore.addEventListener("click",()=>{
+  searchImages();
+});
